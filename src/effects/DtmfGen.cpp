2dc8abd380cc089189c8ac24b12452db5058fd13
Nicholas Gregorich <nicholas.gregorich@juul.com>
2020-02-18 15:09:27 -0800

    Always fade in / out DTMF tone
    When numRemaining is modulated, total != len. This causes the fade out envelope to not be applied. The fade in / out is probably always desired.

--- src/effects/DtmfGen.cpp
+++ src/effects/DtmfGen.cpp
@@ -579,25 +579,20 @@ bool EffectDtmf::MakeDtmfTone(float *buffer, size_t len, float fs, wxChar tone, 
    }
 
    // generate a fade-in of duration 1/250th of second
-   if (last == 0) {
-      A = (fs / kFadeInOut);
-      for(size_t i = 0; i < A; i++) {
-         buffer[i] *= i/A;
-      }
+   A = (fs / kFadeInOut);
+   for(size_t i = 0; i < A; i++) {
+      buffer[i] *= i/A;
    }
 
-   // generate a fade-out of duration 1/250th of second
-   if (last == total - len) {
-      // we are at the last buffer of 'len' size, so, offset is to
-      // backup 'A' samples, from 'len'
-      A = (fs / kFadeInOut);
-      auto offset = long(len) - long(fs / kFadeInOut);
-      // protect against negative offset, which can occur if too a
-      // small selection is made
-      if (offset >= 0) {
-         for(size_t i = 0; i < A; i++) {
-            buffer[i + offset] *= (1 - (i / A));
-         }
+   // we are at the last buffer of 'len' size, so, offset is to
+   // backup 'A' samples, from 'len'
+   A = (fs / kFadeInOut);
+   auto offset = long(len) - long(fs / kFadeInOut);
+   // protect against negative offset, which can occur if too a
+   // small selection is made
+   if (offset >= 0) {
+      for(size_t i = 0; i < A; i++) {
+         buffer[i + offset] *= (1 - (i / A));
       }
    }
    return true;
